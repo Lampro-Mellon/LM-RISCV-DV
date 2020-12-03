79234b61f40b784515221a83386c230b6a5e91b4
behzadmehmood-lm <behzad.mehmood@lampromellon.com>
2020-12-03 22:54:15 +0500

    Updated to RVDV latest commit Commit ID: `cff4f2558006dc3a292c250d6bf0d744d4eff0bd` Major changes: a) `asm_tests` and `ctests` changed to `asm_test` and `ctest` in arguments b)  Files added and updated in `css`, `doctrees`, `fonts`, `RobotoSlab` and `js`

--- scripts/core_integrate.sh
+++ scripts/core_integrate.sh
@@ -5,7 +5,7 @@ BASE_DIR=$(cd ${SCRIPT_PATH}; cd ..; pwd)
 echo "BASE_DIR = ${BASE_DIR}"
 
 RV_DV_REMOTE='https://github.com/google/riscv-dv.git'
-RV_DV_COMMIT_SHA='fb4728e0c1e94e8dbccc5255b42ebdb2e41c63da'
+RV_DV_COMMIT_SHA='cff4f2558006dc3a292c250d6bf0d744d4eff0bd'
 
 SWERV_REMOTE='https://github.com/chipsalliance/Cores-SweRV.git'
 SWERV_COMMIT_SHA='7332edc0adaa7e9a0c842d169154429e8d987786'
